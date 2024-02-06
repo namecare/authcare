687208ca4f6281220155a0dd48f9208a58357d7b
tikhop <tikhop@gmail.com>
2024-02-06 16:13:14 +0100

    chore: Fix typo

--- lib/src/constants.rs
+++ lib/src/constants.rs
@@ -1,5 +1,5 @@
-pub const JWT_AUD_CLAIN: &str = "user";
-pub const JWT_ISS_CLAIN: &str = "authcare-v1";
+pub const JWT_AUD_CLAIM: &str = "user";
+pub const JWT_ISS_CLAIM: &str = "authcare-v1";
 pub const JWT_EXPIRED_IN: i64 = 60; //Minutes
 
 pub const TOKEN_TYPE: &str = "bearer";
